c6e36dcb976bb1ac708f44db61f3d63dc8567d44
Laharisikakollu <amlaharisikakollu@gmail.com>
2020-01-08 11:05:19 +0000

    optimised business application and added exports

---
+++ business.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+  return a + b;
+}
+function sub(a, b) {
+  return a - b;
+}
+function profit(cp, sp) {
+
+  return sub(sp, cp)
+
+}
+
+
+module.exports = {add,sub,profit}
--- index.js
+++ index.js
@@ -1,21 +1,8 @@
-function add(a,b)
-{
-  return a+b;
-}
-function sub(a,b)
-{
-  return a-b;
-}
-var c=add(10,20);
-console.log(c)
-d=sub(30,20);
-console.log(d)
 
-function profit(cp,sp)
-{
-  
-    return sub(sp,cp)
-  
-}
-e=profit(40,50)
+var r=require ('./business')
+var c=r.add(10,20);
+console.log(c)
+d=r.sub(30,20);
+console.log(d)
+e=r.profit(40,50)
 console.log(e)
