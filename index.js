bd04d110df282f45d3d8c471ab7b8e957d159da5
Laharisikakollu <amlaharisikakollu@gmail.com>
2020-01-08 11:15:43 +0000

    add sum function and create math.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 
-var r=require ('./business')
+/*var r=require ('./business')
 var c=r.add(10,20);
 console.log(c)
 d=r.sub(30,20);
 console.log(d)
 e=r.profit(40,50)
-console.log(e)
+console.log(e)*/
+
+var myarr=[1,2,3,4,5,6,7,8]
+
+var m=require('./math')
+
+sum=m.sum(myarr)
+
+console.log(sum)
---
+++ math.js
@@ -0,0 +1,10 @@
+module.exports={
+  sum:function(myarr){
+    var s=0;
+    for(var i=0;i<myarr.length;i++)
+    {
+      s=s+myarr[i]
+    }
+    return s;
+  }
+}
