3babcb649062d4183b54e25e4b87810656b4f63c
Laharisikakollu <amlaharisikakollu@gmail.com>
2020-01-08 10:50:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function add(a,b)
+{
+  return a+b;
+}
+function sub(a,b)
+{
+  return a-b;
+}
+var c=add(10,20);
+console.log(c)
+d=sub(30,20);
+console.log(d)
+
+function profit(cp,sp)
+{
+  
+    return sub(sp,cp)
+  
+}
+e=profit(40,50)
+console.log(e)
